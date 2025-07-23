2f2a49b1818da0cf60e3dcd579dc7f147c3b72e3
Filip-Mosz <filip.moszczynski@onet.pl>
2025-07-23 20:19:37 +0200

    game can be lost

--- game.js
+++ game.js
@@ -1,10 +1,16 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js';
+import { update as updateSnake, draw as drawSnake, getSnakeHead, snakeIntersection, SNAKE_SPEED } from './snake.js';
 import { update as updateFood, draw as drawFood } from './food.js';
+import { outsideGrid } from './grid.js';
 
 let lastRenderTime = 0;
+let gameOver = false;
 const gameBoard = document.getElementById('game-board');
 
 function main(currentTime){
+    if (gameOver){
+        return alert('you lose');
+    }
+
     window.requestAnimationFrame(main);
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
     if (secondsSinceLastRender < 1 / SNAKE_SPEED) {
@@ -27,4 +33,9 @@ function render(){
 function update(){
     updateSnake();
     updateFood();
+    checkDeath();
+}
+
+function checkDeath(){
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
