78fdfc05a5b32ccd87ca6f9ff8ee2d3581b07f60
Filip-Mosz <filip.moszczynski@onet.pl>
2025-07-23 19:54:44 +0200

    food position randimised

---
+++ grid.js
@@ -0,0 +1,7 @@
+export function randomGridPosition(){
+const GRID_SIZE = 21
+    return {
+        x: Math.floor(Math.random() * GRID_SIZE) + 1,
+        y: Math.floor(Math.random() * GRID_SIZE) + 1
+    }
+}
